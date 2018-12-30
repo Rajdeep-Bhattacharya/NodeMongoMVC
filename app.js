9e63323c0cb83143cc206eb6a067db26dcafdc03
rajdeep_bhattacharya <rajdeep1110bhattacharya@gmail.com>
2018-12-30 20:01:12 +0530

    adding simple model , view , controllers

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyP = require('body-parser');
 const app  = express();
 app.use(bodyP.json());
 app.use(bodyP.urlencoded({extended: false}));
-
+const product = require('./routes/product.routes');
+app.use('/products',product);
 var port = process.env.server_port;
 console.log(port);
 app.listen(port,()=>{console.log(`server listening on ${port}`);});
---
+++ controllers/productController.js
@@ -0,0 +1,5 @@
+const model = require('../models/product.model');
+
+exports.test = function(req,res){
+    res.send('greetings from test controller');
+}
